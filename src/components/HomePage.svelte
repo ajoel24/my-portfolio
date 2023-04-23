086a141858fd740593a53ad5f17aa8b923d869dc
ajoel24 <drew.devops@gmail.com>
2023-04-23 19:39:12 +0530

    Install Svelte UI library

---
+++ src/components/HomePage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let name: string
+    import {Card} from '@svelteuidev/core'
+</script>
+
+<Card>
+  <h1>Hello! {name}</h1>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import {add} from '../lib/arithmetic'
+    import HomePage from '../components/HomePage.svelte'
 </script>
 
-<h1>Hello, World!</h1>
-<h2>Sum of 1 and 2 is {add(1, 2)}</h2>
-<h3>Hello, World! Once again!</h3>
-<h3>Hello, World! Once again Once again!</h3>
+<HomePage name="World" />
